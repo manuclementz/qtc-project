b897b94b5a698a2e3cf17e7fdd42469173aa780e
Manu Clementz <manu.clementz@gmail.com>
2024-11-29 15:41:00 +0100

    Animation update

--- static/js/qtc.js
+++ static/js/qtc.js
@@ -1,91 +1,97 @@
 function setupSnowflakes() {
-    var particleCount = 500
-    var particleMax = 2000
-    var sky = document.querySelector('body')
-    var canvas = document.createElement('canvas')
-    canvas.style = 'pointer-events:none';
+    var particleCount = 500;
+    var particleMax = 2000;
+    var sky = document.querySelector('body');
+    var canvas = document.createElement('canvas');
+    canvas.style = 'pointer-events:none;';
     canvas.style.zIndex = -1;
-    var ctx = canvas.getContext('2d')
-    var width = sky.clientWidth
-    var height = Math.max(document.body.scrollHeight, document.body.offsetHeight,
-        document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight);
+    var ctx = canvas.getContext('2d');
+    var width = sky.clientWidth;
+    var height = Math.max(
+        document.body.scrollHeight, document.body.offsetHeight,
+        document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight
+    );
 
-    var i = 0
-    var active = false
-    var snowflakes = []
-    var snowflake
+    var i = 0;
+    var active = false;
+    var snowflakes = [];
+    var snowflake;
+    var lastFrameTime = performance.now(); // Timestamp of the last frame
 
-    canvas.style.position = 'absolute'
-    canvas.style.left = canvas.style.top = '0'
+    canvas.style.position = 'absolute';
+    canvas.style.left = canvas.style.top = '0';
 
     var Snowflake = function () {
-        this.x = 0
-        this.y = 0
-        this.vy = 0
-        this.vx = 0
-        this.r = 0
+        this.x = 0;
+        this.y = 0;
+        this.vy = 0;
+        this.vx = 0;
+        this.r = 0;
 
-        this.reset()
-    }
+        this.reset();
+    };
 
     Snowflake.prototype.reset = function () {
-        this.x = Math.random() * width
-        this.y = Math.random() * -height
-        this.vy = 1 + Math.random() * 1.5
-        this.vx = 0.5 - Math.random()
-        this.r = 1 + Math.random() * 2
-        this.o = 0.5 + Math.random() * 0.5
-    }
+        this.x = Math.random() * width;
+        this.y = Math.random() * -height;
+        this.vy = 50 + Math.random() * 30; // Speed in pixels per second
+        this.vx = (0.5 - Math.random()) * 50; // Horizontal speed in pixels per second
+        this.r = 1 + Math.random() * 2;
+        this.o = 0.5 + Math.random() * 0.5;
+    };
 
     function generateSnowFlakes() {
-        snowflakes = []
+        snowflakes = [];
         for (i = 0; i < particleMax; i++) {
-            snowflake = new Snowflake()
-            snowflake.reset()
-            snowflakes.push(snowflake)
+            snowflake = new Snowflake();
+            snowflake.reset();
+            snowflakes.push(snowflake);
         }
     }
 
-    generateSnowFlakes()
+    generateSnowFlakes();
 
-    function update() {
-        ctx.clearRect(0, 0, width, height)
+    function update(timestamp) {
+        var deltaTime = (timestamp - lastFrameTime) / 1000; // Time since the last frame in seconds
+        lastFrameTime = timestamp;
+
+        ctx.clearRect(0, 0, width, height);
 
         if (!active) {
-            return
+            return;
         }
 
         for (i = 0; i < particleCount; i++) {
-            snowflake = snowflakes[i]
-            snowflake.y += snowflake.vy
-            snowflake.x += snowflake.vx
+            snowflake = snowflakes[i];
+            snowflake.y += snowflake.vy * deltaTime; // Adjusted for deltaTime
+            snowflake.x += snowflake.vx * deltaTime; // Adjusted for deltaTime
 
-            ctx.globalAlpha = snowflake.o
-            ctx.beginPath()
-            ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false)
-            ctx.closePath()
-            ctx.fill()
+            ctx.globalAlpha = snowflake.o;
+            ctx.beginPath();
+            ctx.arc(snowflake.x, snowflake.y, snowflake.r, 0, Math.PI * 2, false);
+            ctx.closePath();
+            ctx.fill();
 
             if (snowflake.y > height) {
-                snowflake.reset()
+                snowflake.reset();
             }
         }
 
-        requestAnimFrame(update)
+        requestAnimFrame(update);
     }
 
     function onResize() {
-        width = sky.clientWidth
-        height = sky.clientHeight
-        canvas.width = width
-        canvas.height = height
-        ctx.fillStyle = '#FFF'
+        width = sky.clientWidth;
+        height = sky.clientHeight;
+        canvas.width = width;
+        canvas.height = height;
+        ctx.fillStyle = '#FFF';
 
-        var wasActive = active
-        active = width > 300
+        var wasActive = active;
+        active = width > 300;
 
         if (!wasActive && active) {
-            requestAnimFrame(update)
+            requestAnimFrame(update);
         }
     }
 
@@ -95,16 +101,17 @@ function setupSnowflakes() {
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             function (callback) {
-                window.setTimeout(callback, 1000 / 60)
+                window.setTimeout(callback, 1000 / 60);
             }
-        )
-    })()
+        );
+    })();
 
-    onResize()
-    window.addEventListener('resize', onResize, false)
+    onResize();
+    window.addEventListener('resize', onResize, false);
 
-    sky.appendChild(canvas)
+    sky.appendChild(canvas);
 }
+
 window.onload = function () {
     setTimeout(setupSnowflakes, 500);
 };
